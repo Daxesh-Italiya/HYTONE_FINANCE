a62bc610ee4874ca11281ae99b543b3fcb90a741
apple <dharmikratanpara25@gmail.com>
2021-12-27 23:37:05 +0530

    Call log feature added

--- app/src/main/java/com/tst/hytonefinance/MainActivity.java
+++ app/src/main/java/com/tst/hytonefinance/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CallLog;
+import android.provider.Telephony;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +25,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myTextView = findViewById(R.id.textview);
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS, Manifest.permission.READ_CALL_LOG}, PackageManager.PERMISSION_GRANTED);
     }
 
-    public void Read_SMS(View view) {
-        Cursor cursor = getContentResolver().query(Uri.parse("content://sms"), null, null, null, null);
-        cursor.moveToFirst();
-        myTextView.setText(cursor.getString(12));
+    public void readSMS(View view) {
+        Cursor getAllSms = getContentResolver().query(Uri.parse("content://sms"), null, null, null, null);
+        getAllSms.moveToFirst();
+        myTextView.setText(getAllSms.getString(12));
+        getAllSms.close();
+    }
+
+    public void readCallLog(View view) {
+
+        StringBuffer sb = new StringBuffer();
+        Cursor managedCursor = managedQuery( CallLog.Calls.CONTENT_URI,null, null,null, null);
+        int number = managedCursor.getColumnIndex( CallLog.Calls.NUMBER );
+        int type = managedCursor.getColumnIndex( CallLog.Calls.TYPE );
+        int date = managedCursor.getColumnIndex( CallLog.Calls.DATE);
+        int duration = managedCursor.getColumnIndex( CallLog.Calls.DURATION);
+        sb.append( "Call Details :");
+        while ( managedCursor.moveToNext() ) {
+            String phNumber = managedCursor.getString( number );
+            String callType = managedCursor.getString( type );
+            String callDate = managedCursor.getString( date );
+            Date callDayTime = new Date(Long.valueOf(callDate));
+            String callDuration = managedCursor.getString( duration );
+            String dir = null;
+            int dircode = Integer.parseInt( callType );
+            switch( dircode ) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    dir = "OUTGOING";
+                    break;
+
+                case CallLog.Calls.INCOMING_TYPE:
+                    dir = "INCOMING";
+                    break;
+
+                case CallLog.Calls.MISSED_TYPE:
+                    dir = "MISSED";
+                    break;
+            }
+            sb.append( "\nPhone Number:--- "+phNumber +" \nCall Type:--- "+dir+" \nCall Date:--- "+callDayTime+" \nCall duration in sec :--- "+callDuration );
+            sb.append("\n----------------------------------");
+        }
+        managedCursor.close();
+        myTextView.setText(sb);
     }
 }
